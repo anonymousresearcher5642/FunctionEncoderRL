746a6e9899a92fe170f322e309f06d281ae14e11
anonymousresearcher5642 <156457338+anonymousresearcher5642@users.noreply.github.com>
2024-05-09 09:22:16 -0500

    Delete HiddenParamSysID directory

--- HiddenParamSysID/run_ablation.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-python 4.train_predictors.py --model_type FE --seed 0
-python 4.train_predictors.py --model_type FE_PWN --seed 0
-python 4.train_predictors.py --model_type FE_F1 --seed 0
-python 4.train_predictors.py --model_type FE_Dif --seed 0
